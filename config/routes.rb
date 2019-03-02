7ee03f5cab61b6f664a50df117f968c08095b223
vengatramanan <vengatramanan26@gmail.com>
2019-03-02 09:31:48 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+resources :blogs
+
+get 'about', to: 'blogs#about_change'
+
+root 'blogs#index'
+end
